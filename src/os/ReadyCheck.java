12b7a05accb3d37560d40fc1e1916b72ddd5cad2
Anton <Anton@Anton-PC.home>
2015-03-31 00:22:13 -0400

    initial set up

---
+++ src/os/ReadyCheck.java
@@ -0,0 +1,11 @@
+package os;
+
+/**
+ *
+ * @author Anton
+ */
+public class ReadyCheck {
+    //class created for propose of testing whether people can commit to the repo
+    //add you name here and try to commit
+    //Anton Begeima
+}
