16e9f68b90c2eef09ad2cea48b00e2db28bf0580
Anton <Anton@Anton-PC.home>
2015-04-06 17:55:27 -0400

    initial creation

---
+++ src/osProject/Job.java
@@ -0,0 +1,16 @@
+/**
+ * Class will represent a single job and will contain all operations performed
+ * on job
+ * Later program will create a job table to store job objects
+
+*/
+package osProject;
+
+/**
+ *
+ * @author Anton
+ */
+public class Job {
+    //TODO: declare variables and write accessors and mutators
+    
+}
