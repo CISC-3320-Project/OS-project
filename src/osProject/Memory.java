ddca7cf864a0773ab5f06b4a10d8888f594508b1
Anton <Anton@Anton-PC.home>
2015-04-07 13:11:43 -0400

    initial creation

---
+++ src/osProject/Memory.java
@@ -0,0 +1,15 @@
+/**
+ * Class will represent memory and functions on it.
+ * Should be able to add to the memory
+ * delete from the memory
+ * find free space for job
+ */
+package osProject;
+
+/**
+ *
+ * @author Anton
+ */
+public class Memory {
+    
+}
