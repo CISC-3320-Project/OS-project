4a5d207423417c209da887b656286acd78cfeab2
antonb03 <begeima@gmail.com>
2015-04-14 14:07:22 -0400

    add function implementation

--- src/osProject/MemoryManager.java
+++ src/osProject/MemoryManager.java
@@ -13,6 +13,7 @@ import java.util.List;
  *
  * @author Anton
  */
+//TODO Anton
 public class MemoryManager {
     private final int MAX_SIZE = 100;
     private List<Integer> memory;
@@ -23,23 +24,22 @@ public class MemoryManager {
             memory.add(0);
     }
     
-    public void addToMemory(Job j){
+    public boolean addToMemory(Job j){
         int sizeNeeded = j.getJobSize();
         boolean startAddSet = false;
         int startAdd = -1;
         int endAdd;
         //int space = 0;
-        for(int i = 0; i<MAX_SIZE; i++){
+        for(int i = 0; i<MAX_SIZE; i++){ //find spase in memory for job
             if(memory.get(i)==0 && startAddSet==false){
                 startAdd = i;
                 startAddSet = true;
-                //space++;
             }
             if((memory.get(i)==1 && startAddSet) || i == 99){
                 endAdd = i;
-                if((endAdd-startAdd)>=sizeNeeded){
-                    j.setAddress(startAdd);
-                    j.setInMemory(true);
+                if((endAdd-startAdd)>=sizeNeeded){ //check if right size for our job
+                    j.setAddress(startAdd); //assign starting address for tis job
+                    //j.setInMemory(true);
                     break;
                 }
                 else
@@ -47,41 +47,16 @@ public class MemoryManager {
                 
             }   
         }
-        if(startAddSet)
+        if(startAddSet){
             for(int i = j.getAddress(); i<j.getAddress()+j.getJobSize(); i++)
                 memory.set(i, 1);
-        
+        return true;
+        }
+        return false;  
     }
     
     public void removeFromMemory(Job j){
         for(int i = j.getAddress(); i<j.getAddress()+j.getJobSize(); i++)
             memory.set(i, 0);
-    }
-    
-    public String toString(){
-        String s = new String();
-        for(int i=0; i<MAX_SIZE; i++)
-            s+=memory.get(i) + " ";
-        return s;
-    }
-
-
-    public static void main(String [] args){
-        MemoryManager mem = new MemoryManager();
-        Job j = new Job(1,1,10,12,12);
-        Job j1 = new Job(2,1,2,12,12);
-        Job j2 = new Job(3,1,70,12,12);
-        Job j3 = new Job(4,1,3,12,12);
-        mem.addToMemory(j);
-        mem.addToMemory(j1);
-        mem.addToMemory(j2);
-        System.out.println(mem.toString());
-        mem.removeFromMemory(j1);
-        System.out.println(mem.toString());
-        mem.addToMemory(j3);
-        System.out.println(mem.toString());
-        //mem.removeFromMemory(j);
-        //System.out.println(mem.toString());
-
     }
 }
